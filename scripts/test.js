2f7afdb24213ad5e89a455e61cebf0348d778f23
Bogdan Mart <mart.bogdan@gmail.com>
2017-10-09 23:34:13 +0300

    Refactored code. Display "<-" for rus language, to indicate that is is current.

--- scripts/test.js
+++ scripts/test.js
@@ -5,38 +5,41 @@ function getLink(collection, lang)
 		var out = null;
 		var pattern;
 		var srctitle;
-		var ua = "https://cdn3.iconfinder.com/data/icons/finalflags/16/Ukraine-Flag.png";
-		var en = "https://cdn2.iconfinder.com/data/icons/flags/flags/16/united-kingdom-great-britain.png";
-		var ru = "https://cdn1.iconfinder.com/data/icons/famfamfam_flag_icons/ru.png";
-		
+		var langImg = {
+			ua: "https://cdn3.iconfinder.com/data/icons/finalflags/16/Ukraine-Flag.png",
+			en: "https://cdn2.iconfinder.com/data/icons/flags/flags/16/united-kingdom-great-britain.png",
+			ru: "https://cdn1.iconfinder.com/data/icons/famfamfam_flag_icons/ru.png",
+		};
+
 		switch (lang) {
 		  case "en":
 			pattern = /en/i;
-			srctitle = en;
+			srctitle = langImg.en;
 			break;
 		  case "ru":
 			pattern = /ru/i;
-			srctitle = ru;
+			srctitle = langImg.ru;
 			break;
 		  case "ua":
 			pattern = /uk/i;
-			srctitle = ua;
+			srctitle = langImg.ua;
 			break;
 		  default:
 			pattern = /en/i;
-			srctitle = en;
+			srctitle = langImg.en;
 		}
 		
 		for(var i=0; i<collection.length;i++)
-			if(pattern.test(collection[i]))
-				out = " <a href=" + collection[i] + "><img src='" + srctitle + "'/>" + lang + "</a>";
-        return out == null ? 
-			(lang == "ua"
-				? checkParrent(window.location.href, "ua", ua) 
-				: lang == "en" 
-					? checkParrent(window.location.href, "en", en)
-					: checkParrent(window.location.href, "ru", ru)) 
-			: out;
+			if(pattern.test(collection[i])) {
+                out = " <a href=" + collection[i] + "><img src='" + srctitle + "'/>" + lang + "</a>";
+            }
+        if(out){
+        	return out;
+		}
+		if(langImg[lang]){
+        	return checkParrent(window.location.href, lang, langImg[lang]);
+        }
+		return null;
 }
 
 function selectPattern(lang)
@@ -65,7 +68,7 @@ function getDefault(srctitle, lang)
 			if(/ru/i.test(collection[i]))
 				out = " <a href=" + collection[i] + "><img src='" + srctitle + "'/>" + lang + "</a>";
 			
-		
+		out = out || "<img src='" + srctitle + "'/>" + lang;
 		return out;
 }
 
@@ -78,7 +81,12 @@ function checkParrent(url, lang, img)
 	return out;
 }
 
-var h = document.getElementById('firstHeading'); 
-h.innerHTML +=  (getLink(translateTarget, "en") == null ? " " :        getLink(translateTarget, "en"));
-h.innerHTML +=  (getLink(translateTarget, "ua") == null ? " " : ", " + getLink(translateTarget, "ua"));
-h.innerHTML +=  (getLink(translateTarget, "ru") == null ? " " : ", " + getLink(translateTarget, "ru"));
+var h = document.getElementById('firstHeading');
+var linksSection = [
+	getLink(translateTarget, "en"),
+	getLink(translateTarget, "ua"),
+	getLink(translateTarget, "ru")
+	]
+	.filter(function (t) { return !!t; })
+	.join(", ");
+h.innerHTML += linksSection;
